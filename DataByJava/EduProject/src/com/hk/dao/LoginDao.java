ce821f41e4a692bc9b7efaa33677217121f094af
296435342 <296435342@qq.com>
2018-11-05 17:18:32 +0800

    登录注册

--- DataByJava/EduProject/src/com/hk/dao/LoginDao.java
+++ DataByJava/EduProject/src/com/hk/dao/LoginDao.java
@@ -1,13 +1,10 @@
 package com.hk.dao;
 
-import java.util.List;
-
-import com.hk.bean.Admin;
-import com.hk.bean.Regist;
+import com.hk.bean.User;
 
 public interface LoginDao {
-	List<Admin> selectAdmin();
-	Admin  checkAdmin(String admin_user,String admin_pwd);
-	int addUser(Regist regist);
-	Regist userLogin(String user,String pwd);
+
+	
+	int addUser(User user);
+	User userLogin(String name,String pwd);
 }
